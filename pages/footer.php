e015c6623591c907d2cb1c82801eef7450b0e9d7
heidi-luong1109 <heidi.luong1109@yahoo.com>
2021-05-27 11:48:47 +1000

    Init commit

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+      <footer>
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-10">
+            <p>Protected by <strong><a href="https://heidi.net.au/security" target="_blank">The Core SECURITY</a></strong></p>
+          </div>
+        </div>
+      </footer>
+    
+    </div>
+	
+  </body>
+</html>
